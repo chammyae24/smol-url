d4dea5948bccf8ae6d28d111a0f8af19ad1b1d97
chammyae24 <chammyae.zawaud@gmail.com>
2025-02-20 01:55:54 +0630

    fixed: migration

---
+++ supabase/migrations/20250219192522_add_trigger_when_user_created.sql
@@ -0,0 +1,24 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.on_auth_users_row_created()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  INSERT INTO public.users (id, full_name, avatar_url)
+  VALUES (NEW.id, NEW.raw_user_meta_data->>'full_name', NEW.raw_user_meta_data->>'avatar_url');
+  
+  RETURN NEW;
+END;
+$function$
+;
+
+create policy "Enable read access to auth users"
+on "public"."clicks"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+
